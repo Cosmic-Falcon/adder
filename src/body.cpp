24dd65fa143de722ccfdae6d9d7fcf06087e8174
Neelay Junnarkar <neelay.junnarkar@gmail.com>
2016-05-17 19:44:07 -0700

    body declaration

---
+++ src/body.cpp
@@ -0,0 +1,31 @@
+#include "Body.h"
+
+void Body::update(float dt) {
+	glm::vec2 net_force = std::accumulate(_forces.begin(), _forces.end(), glm::vec2{0.f, 0.f});
+	float net_torque = std::accumulate(_torques.begin(), _torques.end(), 0.f);
+	_pos += _vel*dt;
+	if(_mass > 0)
+		_vel += (net_force / _mass)*dt;
+	_ori += _ome*dt;
+	if(_I > 0)
+		_ome += (net_torque / _I)*dt;
+
+	//TODO: Something like _poly.setPosition(_pos[0], _pos[1]);
+
+	if(_forces.size() > 100) {
+		_forces.clear();
+		_forces.push_back(net_force);
+	}
+	if(_torques.size() > 100) {
+		_torques.clear();
+		_torques.push_back(net_torque);
+	}
+}
+
+void Body::add_force(const glm::vec2 &force) {
+	_forces.push_back(force);
+}
+
+void Body::add_torque(const float &torque) {
+	_torques.push_back(torque);
+}
